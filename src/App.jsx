3aeaaeddf4fbe76a3cc99bf7a8bf568c09becf32
facugaitan <facuu_elpitu@hotmail.com>
2024-07-11 18:58:22 -0300

    react first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "./components/NavBar"
+import { ItemListContainer } from "./components/ItemListContainer"
+
+function App() {
+  return <>
+    <NavBar />
+    <ItemListContainer greeting="Adios" />
+  </>
+}
+
+export default App
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,8 @@
+import carrito from '../assets/6811628.png';
+
+export const CartWidget = () => ( 
+<>
+<img src={carrito} height={50}/> 
+<span>2</span>
+</>
+);
